8cf24f2b9b6a022d9823c3df457e8d6f0795e726
CToph02 <christopher.munoz.0220@gmail.com>
2026-05-11 01:27:59 -0400

    CToph02: SECRET_KEY changed and added to .env

--- backend/odtman/settings.py
+++ backend/odtman/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4hs!yu160umw_tt+e(3o_w^ji8dmua5+=hx12e99*235(g!*_v'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG') == True
 
 ALLOWED_HOSTS = []
 
