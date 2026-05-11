aed1fca1ed39f983d1a6990b115fc12b9bc4542f
CToph02 <christopher.munoz.0220@gmail.com>
2026-05-11 01:31:55 -0400

    CToph02: Add login, logout and password change empty functions

--- backend/user_auth/views.py
+++ backend/user_auth/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.contrib.auth import login, logout
 # Create your views here.
+
+def user_login(request):
+    pass
+
+def user_logout(request):
+    pass
+
+def user_pwd_change(request):
+    pass
