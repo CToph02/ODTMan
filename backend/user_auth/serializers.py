1bc4b5f72f23a05c22887fe34125057d645b554b
CToph02 <christopher.munoz.0220@gmail.com>
2026-05-11 17:36:52 -0400

    CToph02: Minor modifies.

--- backend/user_auth/serializers.py
+++ backend/user_auth/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 
-User = get_user_model
+User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
@@ -12,8 +12,8 @@ class UserSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         user = User.objects.create_user(
-            username = validated_data['username'],
-            email = validated_data.get('email', ''),
-            password = validated_data['password'],
+            username=validated_data['username'],
+            email=validated_data.get('email', ''),
+            password=validated_data['password']
         )
         return user
