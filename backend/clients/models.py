f92de9ac22ba9a6749659a4af2da9d4bb1a8e1ac
CToph02 <christopher.munoz.0220@gmail.com>
2026-05-13 15:23:20 -0400

    CToph02: Fields for client model added.

--- backend/clients/models.py
+++ backend/clients/models.py
@@ -1,5 +1,12 @@
+from django.db import models
 from odtman.model_base import TimeStampedModel
 
 # Create your models here.
 class Client(TimeStampedModel):
-    pass
+    full_name = models.CharField(max_length=100)
+    email = models.EmailField()
+    address = models.CharField(max_length=200)
+    phone_number = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.full_name}'
