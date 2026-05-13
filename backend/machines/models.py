bcc513efc8615cdc657efe6fd87c3ed82abe9cd0
CToph02 <christopher.munoz.0220@gmail.com>
2026-05-13 15:24:25 -0400

    CToph02: Models for machines app created.

--- backend/machines/models.py
+++ backend/machines/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class MachineBrand(models.Model):
+    brand = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.brand}'
+
+class MachineCategory(models.Model):
+    category = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return f'{self.category}'
+
+class MachineModel(models.Model):
+    model = models.CharField(max_length=100)
+    category = models.ForeignKey(MachineCategory, on_delete=models.CASCADE, related_name='category_models')
+    brand = models.ForeignKey(MachineBrand, on_delete=models.CASCADE, related_name='brand_models')
+    
+    def __str__(self):
+        return f'{self.model} {self.brand}'
+
+class Machine(models.Model):
+    serial_number = models.CharField(max_length=200)
+    model = models.ForeignKey(MachineModel, on_delete=models.CASCADE, related_name='machines')
+    
+    def __str__(self):
+        return f'{self.model} - ({self.serial_number})'
