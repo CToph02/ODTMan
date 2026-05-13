b90321084c321fbf1ad8987339c6eaa3d86d2cf1
Super User <root@topher.localdomain>
2026-05-12 22:18:21 -0400

    CToph02: Remove api/token URL; replaced by custom log_in function in user_auth

--- backend/odtman/urls.py
+++ backend/odtman/urls.py
@@ -21,6 +21,5 @@ from rest_framework_simplejwt.views import (TokenRefreshView, TokenObtainPairVie
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('user_auth.urls')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
