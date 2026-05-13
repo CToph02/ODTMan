0914934acc8ae5b71cb47e832bb5d79d4b3b0bc8
CToph02 <christopher.munoz.0220@gmail.com>
2026-05-13 17:57:32 -0400

    CToph02: Routers registered; Brands and categories url added.

--- backend/machines/urls.py
+++ backend/machines/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import MachineViewSet, MachineModelViewSet
+from .views import MachineViewSet, MachineModelViewSet, MachineBrandViewSet, MachineCategoryViewSet
 
 router = DefaultRouter()
 router.register(r'units', MachineViewSet)
 router.register(r'models', MachineModelViewSet)
+router.register(r'brands', MachineBrandViewSet)
+router.register(r'categories', MachineCategoryViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
