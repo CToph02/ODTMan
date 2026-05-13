e85d9838b01f3809323fb9809fcab8ff033c94ba
CToph02 <christopher.munoz.0220@gmail.com>
2026-05-13 15:23:46 -0400

    CToph02: Models added to admin site.

--- backend/machines/admin.py
+++ backend/machines/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Machine, MachineBrand, MachineCategory, MachineModel
 # Register your models here.
+
+admin.site.register(MachineModel)
+admin.site.register(MachineCategory)
+admin.site.register(MachineBrand)
+admin.site.register(Machine)
