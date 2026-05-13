91454f5d94ad364b1d153722207ef9f8c26a4636
CToph02 <christopher.munoz.0220@gmail.com>
2026-05-13 15:25:30 -0400

    CToph02: ViewSets added to views

--- backend/machines/views.py
+++ backend/machines/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import serializers
+from .serializers import MachineSerializer, MachineModelSerializer
+from .models import Machine, MachineBrand, MachineCategory, MachineModel
 
-# Create your views here.
+class MachineViewSet(viewsets.ModelViewSet):
+    queryset = Machine.objects.all()
+    serializer_class = MachineSerializer
+
+class MachineModelViewSet(viewsets.ModelViewSet):
+    queryset = MachineModel.objects.all()
+    serializer_class = MachineModelSerializer
+
+class BrandViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = MachineBrand.objects.all()
+    serializer_class = serializers.ModelSerializer
+
+class CategoryViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = MachineCategory.objects.all()
+    serializer_class = serializers.ModelSerializer
+    
