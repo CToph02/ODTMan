79e49a2612c3fe8adc2e5d7873331d48455e6450
CToph02 <christopher.munoz.0220@gmail.com>
2026-05-14 13:05:14 -0400

    CToph02: ClientViewSet added.

--- backend/clients/views.py
+++ backend/clients/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import ClientSerializer
+from .models import Client
 
-# Create your views here.
+class ClientViewSet(viewsets.ModelViewSet):
+    queryset = Client.objects.all()
+    serializer_class = ClientSerializer
