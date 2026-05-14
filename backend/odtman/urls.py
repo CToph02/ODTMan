ae4e70b960017b43479c30c86c14f9806c8d09a0
CToph02 <christopher.munoz.0220@gmail.com>
2026-05-14 13:05:56 -0400

    CToph02: URL for clients added.

--- backend/odtman/urls.py
+++ backend/odtman/urls.py
@@ -21,6 +21,7 @@ from rest_framework_simplejwt.views import TokenRefreshView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/machines/', include('machines.urls')),
+    path('api/clients/', include('clients.urls')),
     path('api/auth/', include('user_auth.urls')),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
