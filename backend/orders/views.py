970beb58efca5a659127b65102eed93e219a420a
CToph02 <christopher.munoz.0220@gmail.com>
2026-05-17 00:14:23 -0400

    CToph02: ViewSet for orders app added.

--- backend/orders/views.py
+++ backend/orders/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import OdtOrdersSerializer, PaymentMethodSerializer, RepairStatusSerializer
+from .models import OdtOrders, PaymentMethod, RepairStatus
 
-# Create your views here.
+class OdtOrdersViewSet(viewsets.ModelViewSet):
+    queryset = OdtOrders.objects.all()
+    serializer_class = OdtOrdersSerializer
+
+class PaymentMethodViewSet(viewsets.ModelViewSet):
+    queryset = PaymentMethod.objects.all()
+    serializer_class = PaymentMethodSerializer
+
+class RepairStatusViewSet(viewsets.ModelViewSet):
+    queryset = RepairStatus.objects.all()
+    serializer_class = RepairStatusSerializer
