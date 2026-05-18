1bfa1b049e53a611e14b25794abd2b1d2e517b7e
CToph02 <christopher.munoz.0220@gmail.com>
2026-05-18 15:13:00 -0400

    CToph02: to_representation func added to ModelSerializer, regex for MachineSerializer and CategorySerializer.

--- backend/machines/serializers.py
+++ backend/machines/serializers.py
@@ -2,15 +2,15 @@ from django.core.validators import RegexValidator
 from rest_framework import serializers
 from .models import Machine, MachineBrand, MachineCategory, MachineModel
 
-class MachineModelSerializer(serializers.ModelSerializer):
-    brand_name = serializers.ReadOnlyField(source='brand.brand')
-    category_name = serializers.ReadOnlyField(source='category.category')
-
-    class Meta:
-        model = MachineModel
-        fields = ['id', 'model', 'brand', 'category', 'brand_name', 'category_name']
-
 class MachineSerializer(serializers.ModelSerializer):
+    serial_number = serializers.CharField(
+        validators=[
+            RegexValidator(
+                regex = r"""^[^@'$"´`]+$""",
+                message="El número serial no puede contener catacteres especiales."
+            )
+        ]
+    )
     class Meta:
         model = Machine
         fields = ['id', 'model', 'serial_number']
@@ -19,7 +19,7 @@ class MachineBrandSerializer(serializers.ModelSerializer):
     brand = serializers.CharField(
         validators=[
             RegexValidator(
-                regex = '^[A-Za-zÁÉÍÓÚáéíóúÑñ0-9\s]+$',
+                regex = '^[A-Za-zÁÉÍÓÚáéíóúÑñ0-9\s-]+$',
                 message="La marca solo puede contener letras, números y espacios."
             )
         ]
@@ -33,3 +33,30 @@ class MachineCategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = MachineCategory
         fields = ['id', 'category']
+        extra_kwargs = {
+            'category':{
+                'validators': [
+                    RegexValidator(
+                        regex = '^[A-Za-zÁÉÍÓÚáéíóúÑñ0-9\s]+$',
+                        message="La categoría solo puede contener letras, números y espacios."
+                    )
+                ],
+                'error_messages': {
+                    'unique': 'La categoría ya existe.'
+                }
+            }
+        }
+
+class MachineModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MachineModel
+        fields = ['id', 'model', 'brand', 'category']
+        
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+
+        if instance.brand:
+            representation['brand'] = MachineBrandSerializer(instance.brand).data
+        if instance.category:
+            representation['category'] = MachineCategorySerializer(instance.category).data
+        return representation
