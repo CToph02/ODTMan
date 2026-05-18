8975d8ed91ddea489012bd5acde0318e7761cab9
CToph02 <christopher.munoz.0220@gmail.com>
2026-05-18 15:14:36 -0400

    CToph02: destroy func override un MachineBrandViewSet

--- backend/machines/views.py
+++ backend/machines/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 from .serializers import MachineSerializer, MachineModelSerializer, MachineCategorySerializer, MachineBrandSerializer
 from .models import Machine, MachineBrand, MachineCategory, MachineModel
 
@@ -8,7 +9,7 @@ class MachineViewSet(viewsets.ModelViewSet):
 
 class MachineModelViewSet(viewsets.ModelViewSet):
     queryset = MachineModel.objects.all()
-    serializer_class = MachineModelSerializer
+    serializer_class = MachineModelSerializer    
 
 class MachineBrandViewSet(viewsets.ModelViewSet):
     serializer_class = MachineBrandSerializer
@@ -17,7 +18,13 @@ class MachineBrandViewSet(viewsets.ModelViewSet):
         if self.request.user.is_anonymous:
             return MachineBrand.objects.none()
         return MachineBrand.objects.all()
-
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.delete()
+        return Response(
+            {'message': 'La marca ha sido eliminada correctamente'},
+            status=status.HTTP_200_OK
+        )
 class MachineCategoryViewSet(viewsets.ModelViewSet):
     queryset = MachineCategory.objects.all()
     serializer_class = MachineCategorySerializer
