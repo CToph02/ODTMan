4a321ff21b3cedfa981489f501b66e0bea4e31a7
CToph02 <christopher.munoz.0220@gmail.com>
2026-05-26 16:23:42 -0400

    CToph02: The Mixin now are generic and not for machines only

--- backend/machines/views.py
+++ backend/machines/views.py
@@ -2,27 +2,27 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from .serializers import MachineSerializer, MachineModelSerializer, MachineCategorySerializer, MachineBrandSerializer
 from .models import Machine, MachineBrand, MachineCategory, MachineModel
-from odtman.mixins import MachineMessageResponseMixin
+from odtman.mixins import MessageResponseMixin
 
-class MachineViewSet(MachineMessageResponseMixin, viewsets.ModelViewSet):
+class MachineViewSet(MessageResponseMixin, viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
     queryset = Machine.objects.all()
     serializer_class = MachineSerializer
     verbose_name = "The machine"
 
-class MachineModelViewSet(MachineMessageResponseMixin, viewsets.ModelViewSet):
+class MachineModelViewSet(MessageResponseMixin, viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
     queryset = MachineModel.objects.all()
     serializer_class = MachineModelSerializer
     verbose_name = "The model"
 
-class MachineBrandViewSet(MachineMessageResponseMixin, viewsets.ModelViewSet):
+class MachineBrandViewSet(MessageResponseMixin, viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
     serializer_class = MachineBrandSerializer
     queryset = MachineBrand.objects.all()
     verbose_name = "The brand"
     
-class MachineCategoryViewSet(MachineMessageResponseMixin, viewsets.ModelViewSet):
+class MachineCategoryViewSet(MessageResponseMixin, viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
     queryset = MachineCategory.objects.all()
     serializer_class = MachineCategorySerializer
