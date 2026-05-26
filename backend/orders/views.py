abccebb2550501d9fea61fd04a0b2d6ac7fdaf99
CToph02 <christopher.munoz.0220@gmail.com>
2026-05-26 16:27:22 -0400

    CToph02: Mixin added.

--- backend/orders/views.py
+++ backend/orders/views.py
@@ -1,16 +1,19 @@
-from django.shortcuts import render
 from rest_framework import viewsets
 from .serializers import OdtOrdersSerializer, PaymentMethodSerializer, RepairStatusSerializer
 from .models import OdtOrders, PaymentMethod, RepairStatus
+from odtman.mixins import MessageResponseMixin
 
-class OdtOrdersViewSet(viewsets.ModelViewSet):
+class OdtOrdersViewSet(MessageResponseMixin, viewsets.ModelViewSet):
     queryset = OdtOrders.objects.all()
     serializer_class = OdtOrdersSerializer
+    verbose_name = "The order"
 
-class PaymentMethodViewSet(viewsets.ModelViewSet):
+class PaymentMethodViewSet(MessageResponseMixin, viewsets.ModelViewSet):
     queryset = PaymentMethod.objects.all()
     serializer_class = PaymentMethodSerializer
+    verbose_name = "The payment"
 
-class RepairStatusViewSet(viewsets.ModelViewSet):
+class RepairStatusViewSet(MessageResponseMixin, viewsets.ModelViewSet):
     queryset = RepairStatus.objects.all()
     serializer_class = RepairStatusSerializer
+    verbose_name = "The repair status"
