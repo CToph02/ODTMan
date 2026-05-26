181b76a5215be92d1858f7a5182fbf49496bcd44
CToph02 <christopher.munoz.0220@gmail.com>
2026-05-26 16:27:15 -0400

    CToph02: Mixin added.

--- backend/clients/views.py
+++ backend/clients/views.py
@@ -1,7 +1,9 @@
 from rest_framework import viewsets
 from .serializers import ClientSerializer
 from .models import Client
+from odtman.mixins import MessageResponseMixin
 
-class ClientViewSet(viewsets.ModelViewSet):
+class ClientViewSet(MessageResponseMixin, viewsets.ModelViewSet):
     queryset = Client.objects.all()
     serializer_class = ClientSerializer
+    verbose_name = "The client"
